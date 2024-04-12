fc847d4aaeab8b23b5345375655a2dfa32697e6c
Haruna Bah <harunjibs@gmail.com>
2024-04-12 23:19:59 +0100

    encrypt_password.py

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
-"""A module for encrypting passwords.
+"""A module for encrypt passwd.
 """
 import bcrypt
 
 
 def hash_password(password: str) -> bytes:
-    """Hashes a password using a random salt.
+    """Hashes a passwd using a random salt.
     """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
-    """Checks is a hashed password was formed from the given password.
+    """Checks is a hashed passwd was formed from the given passwd.
     """
     return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
